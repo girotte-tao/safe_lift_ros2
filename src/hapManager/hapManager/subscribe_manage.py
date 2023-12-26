cc5cac638820f3352c08db4f00cf28a9e0e615b1
girotte-tao <taoware@qq.com>
2023-12-26 19:32:22 +0800

    remove pcl

--- src/hapManager/hapManager/subscribe_manage.py
+++ src/hapManager/hapManager/subscribe_manage.py
@@ -1,52 +1,59 @@
 import rclpy
 from rclpy.node import Node
 from livox_ros_driver2.msg import CustomMsg
-from sensor_msgs.msg import PointCloud2
-import pcl  # 引入PCL库
 import numpy as np
+import struct
+import os
 
 class CustomMsgSubscriber(Node):
     def __init__(self):
         super().__init__('custom_msg_subscriber')
         self.subscription = self.create_subscription(
             CustomMsg,
-            '/livox/lidar',
+            '/livox/lidar',  # 替换为实际的话题名称
             self.listener_callback,
             10)
-        self.frames_buffer = []
+        self.frame_data = []
         self.frame_count = 0
+        self.max_frames = 10  # 收集10帧数据
 
     def listener_callback(self, msg):
-        # 处理CustomMsg，转换为点云数据
-        point_cloud = self.convert_to_point_cloud(msg)
-        self.frames_buffer.append(point_cloud)
-        self.frame_count += 1
+        self.get_logger().info('Received message: "%s"' % str(msg))
+        self.process_message(msg)
+        if len(self.frame_data) >= self.max_frames:
+            self.save_and_clear_frame_data()
 
-        # 检查是否收集了足够的帧进行合成
-        if self.frame_count == 10:
-            combined_frame = self.combine_frames(self.frames_buffer)
-            self.save_to_pcd(combined_frame)
-            self.frames_buffer = []
-            self.frame_count = 0
-
-    def convert_to_point_cloud(self, msg):
-        # 将CustomMsg转换为PCL点云数据
+    def process_message(self, msg):
+        # 转换CustomMsg为点云数据
         points = []
         for point in msg.points:
-            points.append([point.x, point.y, point.z])
-        pcl_data = pcl.PointCloud(np.array(points, dtype=np.float32))
-        return pcl_data
+            points.append([point.x, point.y, point.z, point.reflectivity])
+        self.frame_data.append(points)
 
-    def combine_frames(self, frames):
-        # 合成多个帧
-        combined = pcl.PointCloud()
-        for frame in frames:
-            combined += frame
-        return combined
+    def save_and_clear_frame_data(self):
+        # 合并数据并保存为PCD文件
+        merged_points = np.concatenate(self.frame_data, axis=0)
+        pcd_filename = f'frame_{self.frame_count}.pcd'
+        self.save_pcd(merged_points, pcd_filename)
+        # 转换为二进制格式
+        binary_filename = f'frame_{self.frame_count}.bin'
+        self.save_binary(merged_points, binary_filename)
+        # 清除当前帧数据并更新帧计数
+        self.frame_data = []
+        self.frame_count += 1
 
-    def save_to_pcd(self, point_cloud):
-        # 保存点云数据为PCD文件
-        pcl.save(point_cloud, 'combined_frame.pcd')
+    def save_pcd(self, points, filename):
+        # 简单的PCD保存实现，可能需要根据实际的PCD格式进行调整
+        with open(filename, 'w') as f:
+            f.write(f'HEADER .pcd\nPOINTS {len(points)}\nDATA ascii\n')
+            for p in points:
+                f.write(f'{p[0]} {p[1]} {p[2]} {p[3]}\n')
+
+    def save_binary(self, points, filename):
+        # 保存为二进制格式
+        with open(filename, 'wb') as f:
+            for p in points:
+                f.write(struct.pack('fff', p[0], p[1], p[2]))  # 只保存x, y, z
 
 def main(args=None):
     rclpy.init(args=args)
